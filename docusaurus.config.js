3e1f7183c89ed8e6b2088ab4f9fd05d21d6f95df
Kevin Zhang <ztm0929@icloud.com>
2025-02-10 21:28:24 +0800

    Update docusaurus.config.js
    修改baseUrl以匹配腾讯云edgeone产品

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -19,7 +19,7 @@ const config = {
   url: 'https://ztm0929.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/codex-lab',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
