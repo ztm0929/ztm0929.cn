ff9bf050d7722d579f6f092cee8e2cc2ea81fa9a
Kevin Zhang <ztm0929@icloud.com>
2025-03-05 20:47:27 +0800

    Revert "Update docusaurus.config.js"
    This reverts commit 3e1f7183c89ed8e6b2088ab4f9fd05d21d6f95df.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -19,7 +19,7 @@ const config = {
   url: 'https://ztm0929.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/codex-lab',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
