62a957d0ceccfc9cd7a1ec33a17e8e066ea4d315
Kevin Zhang <ztm0929@icloud.com>
2025-06-28 21:34:27 +0800

    feat: 为 DocsPage 组件添加 tableOfContent 属性，设置样式为 'clerk'

--- src/app/docs/[[...slug]]/page.tsx
+++ src/app/docs/[[...slug]]/page.tsx
@@ -19,7 +19,7 @@ export default async function Page(props: {
   const MDXContent = page.data.body;
 
   return (
-    <DocsPage toc={page.data.toc} full={page.data.full}>
+    <DocsPage toc={page.data.toc} full={page.data.full} tableOfContent={{ style: 'clerk'}}>
       <DocsTitle>{page.data.title}</DocsTitle>
       <DocsDescription>{page.data.description}</DocsDescription>
       <DocsBody>
