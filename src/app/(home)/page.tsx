67042def846181fb01773354e16c6497ba4fd139
Kevin Zhang <ztm0929@icloud.com>
2025-06-29 01:07:53 +0800

    feat: 添加备案号码作为 footer

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -2,18 +2,24 @@ import Link from 'next/link';
 
 export default function HomePage() {
   return (
-    <main className="flex flex-1 flex-col justify-center text-center">
-      <h1 className="mb-4 text-2xl font-bold">Hello World</h1>
-      <p className="text-fd-muted-foreground">
-        You can open{' '}
-        <Link
-          href="/docs"
-          className="text-fd-foreground font-semibold underline"
-        >
-          /docs
-        </Link>{' '}
-        and see the documentation.
-      </p>
-    </main>
+      <main className="flex flex-1 flex-col justify-center text-center">
+        <h1 className="mb-4 text-2xl font-bold">Hello World</h1>
+        <p className="text-fd-muted-foreground">
+          You can open{' '}
+          <Link
+            href="/docs"
+            className="text-fd-foreground font-semibold underline"
+          >
+            /docs
+          </Link>{' '}
+          and see the documentation.
+        </p>
+		<footer className="mt-8 text-xs text-fd-muted-foreground flex flex-col items-center">
+			<a href="https://beian.miit.gov.cn/">粤ICP备2024199605号-2</a>
+			<a href="https://beian.mps.gov.cn/#/query/webSearch">
+				粤公网安备44030002005195号
+			</a>
+		</footer>
+      </main>
   );
 }
