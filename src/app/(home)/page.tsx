4f275acc03be17ecbc98d355d380dfce54aae9a7
Kevin Zhang <ztm0929@icloud.com>
2025-06-29 03:10:56 +0800

    feat: 主页添加 Fumadocs 链接

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -2,24 +2,31 @@ import Link from 'next/link';
 
 export default function HomePage() {
   return (
-      <main className="flex flex-1 flex-col justify-center text-center">
-        <h1 className="mb-4 text-2xl font-bold">Hello World</h1>
-        <p className="text-fd-muted-foreground">
-          You can open{' '}
-          <Link
-            href="/docs"
-            className="text-fd-foreground font-semibold underline"
-          >
-            /docs
-          </Link>{' '}
-          and see the documentation.
-        </p>
-		<footer className="mt-8 text-xs text-fd-muted-foreground flex flex-col items-center">
-			<a href="https://beian.miit.gov.cn/">粤ICP备2024199605号-2</a>
-			<a href="https://beian.mps.gov.cn/#/query/webSearch">
-				粤公网安备44030002005195号
-			</a>
-		</footer>
-      </main>
+    <main className="flex flex-1 flex-col justify-center text-center">
+      <h1 className="mb-4 text-2xl font-bold">Hello World</h1>
+      <p className="text-fd-muted-foreground">
+        You can open{' '}
+        <Link
+          href="/docs"
+          className="text-fd-foreground font-semibold underline"
+        >
+          /docs
+        </Link>{' '}
+        and see the documentation.
+      </p>
+      <p className="text-fd-muted-foreground">
+		本站使用{' '}
+		<Link href="https://fumadocs.dev" className="text-fd-foreground font-semibold underline">
+		Fumadocs
+		</Link>{' '}
+		构建 ❤
+		</p>
+      <footer className="mt-8 text-xs text-fd-muted-foreground flex flex-col items-center">
+        <a href="https://beian.miit.gov.cn/">粤ICP备2024199605号-2</a>
+        <a href="https://beian.mps.gov.cn/#/query/webSearch">
+          粤公网安备44030002005195号
+        </a>
+      </footer>
+    </main>
   );
 }
