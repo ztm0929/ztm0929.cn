d3341b98d7c099bdb41176027851c6e09210925b
Kevin Zhang <ztm0929@icloud.com>
2025-06-30 03:13:39 +0800

    feat: 添加 lastUpdate 属性到 DocsPage 组件以显示最后修改时间

--- source.config.ts
+++ source.config.ts
@@ -21,4 +21,5 @@ export default defineConfig({
   mdxOptions: {
     // MDX options
   },
+  lastModifiedTime: 'git',
 });
--- src/app/notes/[[...slug]]/page.tsx
+++ src/app/notes/[[...slug]]/page.tsx
@@ -19,7 +19,7 @@ export default async function Page(props: {
   const MDXContent = page.data.body;
 
   return (
-    <DocsPage toc={page.data.toc} full={page.data.full} tableOfContent={{ style: 'clerk'}}>
+    <DocsPage toc={page.data.toc} full={page.data.full} tableOfContent={{ style: 'clerk' }} lastUpdate={new Date(page.data.lastModified)}>
       <DocsTitle>{page.data.title}</DocsTitle>
       <DocsDescription>{page.data.description}</DocsDescription>
       <DocsBody>
