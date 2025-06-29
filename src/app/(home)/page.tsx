9f73fa57fbd1327289cb14a22f32c222eb1c49b6
Kevin Zhang <ztm0929@icloud.com>
2025-06-30 05:31:51 +0800

    feat: 更新 HomePage 组件，修改欢迎信息和布局样式

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -2,30 +2,32 @@ import Link from 'next/link';
 
 export default function HomePage() {
   return (
-    <main className="flex flex-1 flex-col justify-center text-center">
-      <h1 className="mb-4 text-2xl font-bold">Hello World</h1>
-      <p className="text-fd-muted-foreground">
-        You can open{' '}
-        <Link
-          href="/notes"
-          className="text-fd-foreground font-semibold underline"
-        >
-          /notes
-        </Link>{' '}
-        and see some notes.
-      </p>
-      <p className="text-fd-muted-foreground">
-		本站使用{' '}
-		<Link href="https://fumadocs.dev" className="text-fd-foreground font-semibold underline">
-		Fumadocs
-		</Link>{' '}
-		构建 ❤
-		</p>
-      <footer className="mt-8 text-xs text-fd-muted-foreground flex flex-col items-center">
-        <a href="https://beian.miit.gov.cn/">粤ICP备2024199605号-2</a>
-        <a href="https://beian.mps.gov.cn/#/query/webSearch">
+    <main className="flex flex-1 flex-col justify-between text-center">
+      <div className="flex flex-col justify-center flex-1">
+        <h1 className="mb-4 text-2xl font-bold">欢迎！</h1>
+        <p className="text-fd-muted-foreground">
+          访问{' '}
+          <Link
+            href="/notes"
+            className="text-fd-foreground font-semibold underline"
+          >
+            /notes
+          </Link>{' '}
+          并查看笔记库
+        </p>
+      </div>
+      <footer className="mb-8 text-xs text-fd-muted-foreground flex flex-col items-center gap-2">
+        <p className="text-fd-muted-foreground text-sm">
+          本站使用{' '}
+          <Link href="https://fumadocs.dev" className="text-fd-foreground font-semibold underline">
+          Fumadocs
+          </Link>{' '}
+          构建 ❤
+        </p>
+        <Link href="https://beian.miit.gov.cn/">粤ICP备2024199605号-2</Link>
+        <Link href="https://beian.mps.gov.cn/#/query/webSearch">
           粤公网安备44030002005195号
-        </a>
+        </Link>
       </footer>
     </main>
   );
