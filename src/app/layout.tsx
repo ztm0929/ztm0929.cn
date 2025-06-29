daac58206276c159a795496058792f34f0f7f041
Kevin Zhang <ztm0929@icloud.com>
2025-06-30 03:33:22 +0800

    feat: 更新 RootProvider 组件，添加国际化支持和翻译文本

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,15 @@ export default function Layout({ children }: { children: ReactNode }) {
   return (
     <html lang="en" className={inter.className} suppressHydrationWarning>
       <body className="flex flex-col min-h-screen">
-        <RootProvider search={{ enabled: false }}>{children}</RootProvider>
+        <RootProvider
+			search={{ enabled: false }}
+			i18n={{ locale: 'cn', translations: {
+				lastUpdate: '最后更新于',
+				previousPage: '上一页',
+				nextPage: '下一页'
+			} }}>
+			{children}
+		</RootProvider>
       </body>
     </html>
   );
