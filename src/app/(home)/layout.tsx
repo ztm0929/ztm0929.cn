2f4ffa686bf7138726f26eba9eb56f6db8c91963
Kevin Zhang <ztm0929@icloud.com>
2025-07-01 07:51:38 +0800

    feat: 在布局组件中添加链接属性以增强导航功能

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -3,5 +3,5 @@ import { HomeLayout } from 'fumadocs-ui/layouts/home';
 import { baseOptions } from '@/app/layout.config';
 
 export default function Layout({ children }: { children: ReactNode }) {
-  return <HomeLayout {...baseOptions}>{children}</HomeLayout>;
+  return <HomeLayout {...baseOptions} links={[{ text: '笔记', url: '/notes', secondary: false, active: 'nested-url' }, { text: '博客', url: '/blog', secondary: false, active: 'nested-url' }]}>{children}</HomeLayout>;
 }
