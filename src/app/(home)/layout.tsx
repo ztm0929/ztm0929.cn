2d2529f4cd1874b56d8b6166476b1804d1d9daae
Kevin Zhang <ztm0929@icloud.com>
2025-07-02 07:06:22 +0800

    fix: 更新布局组件中的链接，添加 NaaS 网址

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -3,5 +3,5 @@ import { HomeLayout } from 'fumadocs-ui/layouts/home';
 import { baseOptions } from '@/app/layout.config';
 
 export default function Layout({ children }: { children: ReactNode }) {
-  return <HomeLayout {...baseOptions} links={[{ text: '笔记', url: '/notes', active: 'nested-url' }, { text: '博客', url: '/blog', active: 'nested-url' }, { text: 'Wiki', url: 'https://wiki.ztm0929.cn'}, { text: '关于我', url: '/about', active: 'nested-url' }]}>{children}</HomeLayout>;
+  return <HomeLayout {...baseOptions} links={[{ text: '笔记', url: '/notes', active: 'nested-url' }, { text: '博客', url: '/blog', active: 'nested-url' }, { text: 'Wiki', url: 'https://wiki.ztm0929.cn' }, { text: 'NaaS', url: 'https://ztm0929.net' }, { text: '关于我', url: '/about', active: 'nested-url' }]}>{children}</HomeLayout>;
 }
