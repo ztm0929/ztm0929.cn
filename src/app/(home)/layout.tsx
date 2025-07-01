c37517a965df8ba6caeaeafe0d65480c55190b85
Kevin Zhang <ztm0929@icloud.com>
2025-07-01 09:03:38 +0800

    feat: 更新布局组件的链接属性，添加 Wiki 链接并移除次要标记

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -3,5 +3,5 @@ import { HomeLayout } from 'fumadocs-ui/layouts/home';
 import { baseOptions } from '@/app/layout.config';
 
 export default function Layout({ children }: { children: ReactNode }) {
-  return <HomeLayout {...baseOptions} links={[{ text: '笔记', url: '/notes', secondary: false, active: 'nested-url' }, { text: '博客', url: '/blog', secondary: false, active: 'nested-url' }]}>{children}</HomeLayout>;
+  return <HomeLayout {...baseOptions} links={[{ text: '笔记', url: '/notes', active: 'nested-url' }, { text: '博客', url: '/blog', active: 'nested-url' }, { text: 'Wiki', url: 'https://wiki.ztm0929.cn'}]}>{children}</HomeLayout>;
 }
