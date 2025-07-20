4690aea5afee2b41fe7f7735b04a0fd9cb7e8e40
Kevin Zhang <ztm0929@icloud.com>
2025-07-21 02:58:54 +0800

    feat: 更新 Next.js 配置以支持图像优化设置

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,10 @@ const withMDX = createMDX();
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
+  trailingSlash: true,
 };
 
 export default withMDX(config);
