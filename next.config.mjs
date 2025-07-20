d50eb804cea153fc463e8649340e24d275c99ae6
Kevin Zhang <ztm0929@icloud.com>
2025-07-21 02:26:37 +0800

    chore: 停用静态导出，为 Vercel 部署做准备

--- next.config.mjs
+++ next.config.mjs
@@ -5,11 +5,6 @@ const withMDX = createMDX();
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
-  output: 'export',
-  images: {
-	unoptimized: true,
-  },
-  trailingSlash: true
 };
 
 export default withMDX(config);
