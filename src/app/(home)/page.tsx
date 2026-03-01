55ceb601faa574b32511014439f920485002291e
Kevin Zhang <ztm0929@icloud.com>
2026-03-01 20:51:39 +0000

    chore: 微调首页页脚

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -18,17 +18,7 @@ export default function HomePage() {
         </p>
       </div>
       <footer className="mb-8 text-xs text-fd-muted-foreground flex flex-col items-center gap-2">
-        <p className="text-fd-muted-foreground text-sm">
-          本站使用{' '}
-          <Link href="https://fumadocs.dev" className="text-fd-foreground font-semibold underline" target='_blank' rel="noopener noreferrer">
-          Fumadocs
-          </Link>{' '}
-          构建 ❤
-        </p>
-        <Link href="https://beian.miit.gov.cn/" target='_blank' rel="noopener noreferrer">粤ICP备2024199605号-2</Link>
-        <Link href="https://beian.mps.gov.cn/#/query/webSearch" target='_blank' rel="noopener noreferrer">
-          粤公网安备44030002005195号
-        </Link>
+        <Link href="https://beian.miit.gov.cn/" target='_blank' rel="noopener noreferrer">粤 ICP 备 2024199605 号 | ztm0929 © 2026</Link>
       </footer>
     </main>
   );
