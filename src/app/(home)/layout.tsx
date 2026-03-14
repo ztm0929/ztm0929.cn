25edae3713f07c2525ea0ce85032730ac811dd9a
Kevin Zhang <ztm0929@icloud.com>
2026-03-14 13:00:18 +0800

    chore: 微调路径结构

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -7,12 +7,7 @@ export default function Layout({ children }: { children: ReactNode }) {
     <HomeLayout
       {...baseOptions}
       links={[
-        { text: "笔记", url: "/notes", active: "nested-url" },
-        { text: "教程", url: "/tutorials", active: "nested-url" },
-        { text: "博客", url: "/blog", active: "nested-url" },
-        // { text: "Memos", url: "https://memos.ztm0929.cn" },
-        // { text: "Wiki", url: "https://wiki.ztm0929.cn" },
-        // { text: "NaaS", url: "https://ztm0929.net" },
+        { text: "小白教程库", url: "/tutorials", active: "nested-url" },
         { text: "关于我", url: "/about", active: "nested-url" },
       ]}
     >
--- src/app/notes/layout.tsx
+++ src/app/notes/layout.tsx
@@ -7,8 +7,7 @@ import { GraduationCap, Tags } from 'lucide-react';
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <DocsLayout tree={source.pageTree} {...baseOptions} links={[
-      { text: '教程', url: '/tutorials', icon: <GraduationCap /> },
-      { text: '标签', url: '/tags', icon: <Tags /> }
+      { text: '教程', url: '/tutorials', icon: <GraduationCap /> }
     ]}>
       {children}
     </DocsLayout>
