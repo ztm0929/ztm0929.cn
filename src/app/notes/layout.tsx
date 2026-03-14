2f83b5b0978e4637ebb49c6f2470407ff19d5194
Kevin Zhang <ztm0929@icloud.com>
2026-03-14 15:20:57 +0800

    style: 增加侧边 tab

--- src/app/notes/layout.tsx
+++ src/app/notes/layout.tsx
@@ -1,14 +1,21 @@
-import { DocsLayout } from 'fumadocs-ui/layouts/docs';
-import type { ReactNode } from 'react';
-import { baseOptions } from '@/app/layout.config';
-import { source } from '@/lib/source';
-import { GraduationCap, Tags } from 'lucide-react';
+import { DocsLayout } from "fumadocs-ui/layouts/docs";
+import type { ReactNode } from "react";
+import { baseOptions } from "@/app/layout.config";
+import { source } from "@/lib/source";
+import { GraduationCap, Tags } from "lucide-react";
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <DocsLayout tree={source.pageTree} {...baseOptions} links={[
-      { text: '教程', url: '/tutorials', icon: <GraduationCap /> }
-    ]}>
+    <DocsLayout
+      sidebar={{
+        tabs: [
+          { title: "小白教程库", url: "/tutorials", description: "Step-by-step guides to learn the application.", icon: <GraduationCap /> },
+          { title: "杂记", url: "/notes", description: "Random thoughts and notes.", icon: <Tags /> },
+        ],
+      }}
+      tree={source.pageTree}
+      {...baseOptions}
+    >
       {children}
     </DocsLayout>
   );
--- src/app/tutorials/layout.tsx
+++ src/app/tutorials/layout.tsx
@@ -1,13 +1,22 @@
-import { DocsLayout } from 'fumadocs-ui/layouts/docs';
-import type { ReactNode } from 'react';
-import { baseOptions } from '@/app/layout.config';
-import { tutorialsSource } from '@/lib/source';
-import { Notebook } from 'lucide-react';
+import { DocsLayout } from "fumadocs-ui/layouts/docs";
+import type { ReactNode } from "react";
+import { baseOptions } from "@/app/layout.config";
+import { source } from "@/lib/source";
+import { GraduationCap, Tags } from "lucide-react";
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-	<DocsLayout tree={tutorialsSource.pageTree} {...baseOptions} links={[ { text: '笔记', url: '/notes', icon: <Notebook /> } ]}>
-	  {children}
-	</DocsLayout>
+    <DocsLayout
+      sidebar={{
+        tabs: [
+          { title: "小白教程库", url: "/tutorials", description: "Step-by-step guides to learn the application.", icon: <GraduationCap /> },
+          { title: "杂记", url: "/notes", description: "Random thoughts and notes.", icon: <Tags /> },
+        ],
+      }}
+      tree={source.pageTree}
+      {...baseOptions}
+    >
+      {children}
+    </DocsLayout>
   );
 }
