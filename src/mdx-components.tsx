1d0657043f217d6e3a12f87a4e9821c8d75ae82e
Kevin Zhang <ztm0929@icloud.com>
2026-03-19 20:55:34 +0800

    chore: 允许识别静态导入图片对象

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -2,7 +2,17 @@ import defaultMdxComponents from 'fumadocs-ui/mdx';
 import * as TabsComponents from 'fumadocs-ui/components/tabs';
 import { ImageZoom } from 'fumadocs-ui/components/image-zoom';
 import { CodeBlock, Pre } from 'fumadocs-ui/components/codeblock';
+import type { StaticImageData } from 'next/image';
 import type { MDXComponents } from 'mdx/types';
+
+function isStaticImageData(value: unknown): value is StaticImageData {
+  return (
+    typeof value === 'object' &&
+    value !== null &&
+    'src' in value &&
+    typeof (value as { src: unknown }).src === 'string'
+  );
+}
 
 // use this function to get MDX components, you will need it for rendering MDX
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
@@ -10,7 +20,12 @@ export function getMDXComponents(components?: MDXComponents): MDXComponents {
     ...defaultMdxComponents,
     pre: ({ ref: _ref, ...props }) => <CodeBlock {...props}><Pre>{props.children}</Pre></CodeBlock>,
     img: ({ src, ...props }) => {
-      if (typeof src !== 'string' && typeof src !== 'undefined') return null;
+      const isSupportedSrc =
+        typeof src === 'string' ||
+        typeof src === 'undefined' ||
+        isStaticImageData(src);
+
+      if (!isSupportedSrc) return null;
 
       return <ImageZoom {...props} src={src} />;
     },
