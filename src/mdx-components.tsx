c27530603da1b82f90775d9dff2ddfed45c6caa5
Kevin Zhang <ztm0929@icloud.com>
2026-04-05 03:29:01 +0800

    feat: 添加“草稿提示组件”

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -1,10 +1,26 @@
 import defaultMdxComponents from 'fumadocs-ui/mdx';
 import * as TabsComponents from 'fumadocs-ui/components/tabs';
 import * as AccordionComponents from 'fumadocs-ui/components/accordion';
+import { Callout } from 'fumadocs-ui/components/callout';
 import { ImageZoom } from 'fumadocs-ui/components/image-zoom';
 import { CodeBlock, Pre } from 'fumadocs-ui/components/codeblock';
 import type { StaticImageData } from 'next/image';
 import type { MDXComponents } from 'mdx/types';
+import type { ReactNode } from 'react';
+
+function DraftNotice({
+  title = '草稿提示',
+  children = '本文未完工',
+}: {
+  title?: string;
+  children?: ReactNode;
+}) {
+  return (
+    <Callout type="warn" title={title}>
+      {children}
+    </Callout>
+  );
+}
 
 function isStaticImageData(value: unknown): value is StaticImageData {
   return (
@@ -19,6 +35,7 @@ function isStaticImageData(value: unknown): value is StaticImageData {
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
   return {
     ...defaultMdxComponents,
+    DraftNotice,
     pre: ({ ref: _ref, ...props }) => <CodeBlock {...props}><Pre>{props.children}</Pre></CodeBlock>,
     img: ({ src, ...props }) => {
       const isSupportedSrc =
