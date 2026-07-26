8cd7b420b42516db6b78ce43c5573a71131adc7c
Kevin Zhang <ztm0929@icloud.com>
2026-07-27 05:01:36 +0800

    chore: 更新依赖

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,10 @@ const withMDX = createMDX();
 
 /** @type {import('next').NextConfig} */
 const config = {
+  experimental: {
+	useTypeScriptCli: true,
+  },
+
   reactStrictMode: true,
   images: {
 	unoptimized: true,
